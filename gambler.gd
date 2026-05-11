57358aa070f18888dc4f3791e3eee777af58a134
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-05-11 18:47:30 +1200

    1.5hr working on smooth movement from gamble

--- gambler.gd
+++ gambler.gd
@@ -1,8 +1,11 @@
 extends Node2D
 var roll = false
+var fin = Vector2(0,0)
+var him = false
 var a = 0
 var b = 0
 var c = 0
+var d = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,64 +13,73 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if roll == true:
-		a = 0
-		b = 0
-		c = 0
-		roll = false
-		await get_tree().create_timer(1.0).timeout
-		a = randi_range(1,3)
-		b = randi_range(1,3)
-		c = randi_range(1,3)
-	if a == 1:
-		$Node2D/first/seven.play('seven')
-		$Node2D/first/apple.play('no')
-		$Node2D/first/grape.play('no')
-	elif a == 2:
-		$Node2D/first/seven.play('no')
-		$Node2D/first/apple.play('apple')
-		$Node2D/first/grape.play('no')
-	elif a == 3:
-		$Node2D/first/seven.play('no')
-		$Node2D/first/apple.play('no')
-		$Node2D/first/grape.play('grape')
-	else:
-		$Node2D/first/seven.play('no')
-		$Node2D/first/apple.play('no')
-		$Node2D/first/grape.play('no')
-	if b == 1:
-		$Node2D/second/seven.play('seven')
-		$Node2D/second/apple.play('no')
-		$Node2D/second/grape.play('no')
-	elif b == 2:
-		$Node2D/second/seven.play('no')
-		$Node2D/second/apple.play('apple')
-		$Node2D/second/grape.play('no')
-	elif b == 3:
-		$Node2D/second/seven.play('no')
-		$Node2D/second/apple.play('no')
-		$Node2D/second/grape.play('grape')
-	else:
-		$Node2D/second/seven.play('no')
-		$Node2D/second/apple.play('no')
-		$Node2D/second/grape.play('no')
-	if c == 1:
-		$Node2D/third/seven.play('seven')
-		$Node2D/third/apple.play('no')
-		$Node2D/third/grape.play('no')
-	elif c == 2:
-		$Node2D/third/seven.play('no')
-		$Node2D/third/apple.play('apple')
-		$Node2D/third/grape.play('no')
-	elif c == 3:
-		$Node2D/third/seven.play('no')
-		$Node2D/third/apple.play('no')
-		$Node2D/third/grape.play('grape')
-	else:
-		$Node2D/third/seven.play('no')
-		$Node2D/third/apple.play('no')
-		$Node2D/third/grape.play('no')
-
+	if global.crosser == false:
+		if roll == true:
+			him = true
+			a = 0
+			b = 0
+			c = 0
+			roll = false
+			global.him = true
+			await get_tree().create_timer(1.0).timeout
+			global.him = false
+			fin = Vector2(0,0)
+			d += 1
+			a = randi_range(1,3)
+			b = randi_range(1,3)
+			c = randi_range(1,3)
+		if a == 1:
+			$Node2D/first/seven.play('seven')
+			$Node2D/first/apple.play('no')
+			$Node2D/first/grape.play('no')
+		elif a == 2:
+			$Node2D/first/seven.play('no')
+			$Node2D/first/apple.play('apple')
+			$Node2D/first/grape.play('no')
+		elif a == 3:
+			$Node2D/first/seven.play('no')
+			$Node2D/first/apple.play('no')
+			$Node2D/first/grape.play('grape')
+		else:
+			$Node2D/first/seven.play('no')
+			$Node2D/first/apple.play('no')
+			$Node2D/first/grape.play('no')
+		if b == 1:
+			$Node2D/second/seven.play('seven')
+			$Node2D/second/apple.play('no')
+			$Node2D/second/grape.play('no')
+		elif b == 2:
+			$Node2D/second/seven.play('no')
+			$Node2D/second/apple.play('apple')
+			$Node2D/second/grape.play('no')
+		elif b == 3:
+			$Node2D/second/seven.play('no')
+			$Node2D/second/apple.play('no')
+			$Node2D/second/grape.play('grape')
+		else:
+			$Node2D/second/seven.play('no')
+			$Node2D/second/apple.play('no')
+			$Node2D/second/grape.play('no')
+		if c == 1:
+			$Node2D/third/seven.play('seven')
+			$Node2D/third/apple.play('no')
+			$Node2D/third/grape.play('no')
+		elif c == 2:
+			$Node2D/third/seven.play('no')
+			$Node2D/third/apple.play('apple')
+			$Node2D/third/grape.play('no')
+		elif c == 3:
+			$Node2D/third/seven.play('no')
+			$Node2D/third/apple.play('no')
+			$Node2D/third/grape.play('grape')
+		else:
+			$Node2D/third/seven.play('no')
+			$Node2D/third/apple.play('no')
+			$Node2D/third/grape.play('no')
+		if d == 3:
+			global.character_move = false
+			d = 0
+	
 
 
 func _on_roller_body_entered(body: Node2D) -> void:
--- main_character.gd
+++ main_character.gd
@@ -1,19 +1,24 @@
 extends CharacterBody2D
-
+var fin = Vector2(0,0)
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-	if Input.is_action_just_pressed("ui_w") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	var direction := Input.get_axis("ui_a", "ui_d")
-	if direction:
-		velocity.x = direction * 200
+	if global.character_move == true:
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+		if Input.is_action_just_pressed("ui_w") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		var direction := Input.get_axis("ui_a", "ui_d")
+		if direction:
+			velocity.x = direction * 200
+		else:
+			velocity.x = move_toward(velocity.x, 0, 200)
 	else:
-		velocity.x = move_toward(velocity.x, 0, 200)
-
+		velocity.x = 0
+	if global.him == true:
+		fin = $"../../Node2D/move_aside".global_position 
+		$".".global_position = position.move_toward(fin, 300 * delta)
 	move_and_slide()
