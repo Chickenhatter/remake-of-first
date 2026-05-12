7bf6800130880a10e20a4df860dd1b81819a2bcc
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-05-12 18:41:42 +1200

    3.5hr added the ground hit sound and added the sky

--- main_character.gd
+++ main_character.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 var fin = Vector2(0,0)
-
+var tren = false
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -8,7 +8,12 @@ const JUMP_VELOCITY = -400.0
 func _physics_process(delta: float) -> void:
 	if global.character_move == true:
 		if not is_on_floor():
+			tren = true
 			velocity += get_gravity() * delta
+		if is_on_floor():
+			if tren == true:
+				$AudioStreamPlayer2D.play()
+				tren = false
 		if Input.is_action_just_pressed("ui_w") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
 		var direction := Input.get_axis("ui_a", "ui_d")
