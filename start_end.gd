152722960a92036a9b92bda6fd2eeea4836524a9
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-05-13 22:34:04 +1200

    laptop exploded commit

--- start_end.gd
+++ start_end.gd
@@ -1,7 +1,7 @@
 extends Node2D
 var inside = false
 var tim = true
-
+var active = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,12 +11,18 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if tim == true:
 		tim = false
-		await get_tree().create_timer(1.0).timeout
-		global.timer += 1
+		await get_tree().create_timer(0.01).timeout
+		global.timer += 0.01
 		tim = true
 	if inside == true:
 		if Input.is_action_just_pressed('ui_k'):
 			get_tree().change_scene_to_file('res://time_count.tscn')
+	if active == true:
+		active = false
+		$RichTextLabel.add_text("Oh, you are supposed to press K at the start of the game")
+		await get_tree().create_timer(3.0).timeout
+		$RichTextLabel.text = ("")
+		
 
 
 func _on_start_end_body_entered(body: Node2D) -> void:
@@ -26,3 +32,8 @@ func _on_start_end_body_entered(body: Node2D) -> void:
 func _on_start_end_body_exited(body: Node2D) -> void:
 	if body.name == 'main_character':
 		inside = false
+
+
+func _on_message_body_entered(body: Node2D) -> void:
+	if body.name == 'main_character':
+		active = true
